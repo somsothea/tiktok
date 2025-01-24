e707467a8cf1e8a8a8fd3729769431d7e60b2f7e
Som Sothea <somsothea@gmail.com>
2025-01-24 23:28:00 +0700

    first commit

---
+++ lib/constant/data_json.dart
@@ -0,0 +1,54 @@
+List items = [
+  {
+    "videoUrl": "assets/videos/video_1.mp4",
+    "name": "Som Sothea",
+    "caption": "Angkor Wat at sunrise",
+    "songName": "original sound - angkor",
+    "profileImg":
+        "https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/af998be6fb49423fa4dfe648da229722.jpeg",
+    "likes": "1.5M",
+    "comments": "18.9K",
+    "shares": "80K",
+    "albumImg":
+        "https://images.unsplash.com/photo-1502982899975-893c9cf39028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    "videoUrl": "assets/videos/video_2.mp4",
+    "name": "Dara Chamroeun",
+    "caption": "ជើងភ្នំក្រោម #cambodiatiktok",
+    "songName": "original sound - ",
+    "profileImg":
+        "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/ba13e655825553a46b1913705e3a8617.jpeg",
+    "likes": "4.4K",
+    "comments": "5.2K",
+    "shares": "100",
+    "albumImg":
+        "https://images.unsplash.com/photo-1462804512123-465343c607ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+  },
+  {
+    "videoUrl": "assets/videos/video_3.mp4",
+    "name": "Heng Sombo",
+    "caption": "អង្គរវត្ត ខេត្តសៀមរាប",
+    "songName": "original sound - ព្រះសុរិយា",
+    "profileImg":
+        "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/1664576339652610.jpeg",
+    "likes": "100K",
+    "comments": "10K",
+    "shares": "8.5K",
+    "albumImg":
+        "https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+  {
+    "videoUrl": "assets/videos/video_4.mp4",
+    "name": "Sok Sopheak",
+    "caption": "ប្រាសាទតាព្រហ្ម",
+    "songName": "original sound - Temple",
+    "profileImg":
+        "https://p16-tiktokcdn-com.akamaized.net/aweme/720x720/tiktok-obj/1664576339652610.jpeg",
+    "likes": "100K",
+    "comments": "10K",
+    "shares": "8.5K",
+    "albumImg":
+        "https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+  },
+];
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok/pages/root_app.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
+
